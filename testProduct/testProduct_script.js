49b8d8f6737d10dd232837a88b0d69edee2b5ff4
Saad Sheikh <saadsheikh182000@gmail.com>
2023-06-29 00:01:38 -0700

    tmdb fetch request added to testproduct

--- testProduct/testProduct_script.js
+++ testProduct/testProduct_script.js
@@ -19,3 +19,23 @@ const search=()=>{
         }
     }
 }
+
+const resultCardTemplate=document.querySelector("[data-result-template]");
+const resultCardContainer=document.querySelector("[data-result-cards-container]");
+
+//movie db options
+const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJhOTNlY2ZiM2IzNGYyZGRjODhhN2Q5NmJjZjI1YmYyYyIsInN1YiI6IjY0NmVmY2QwMTEzMGJkMDFlYzk3NDgxYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.uCyfm2IkCQO_h839GWVhTZNyRhLMOd78awOjffRmVvk'
+    }
+  };
+  
+  fetch('https://api.themoviedb.org/3/search/movie?query=fight%20club&include_adult=false&language=en&page=1', options)
+    .then(response => response.json())
+    .catch(err => console.error(err))
+    .then(data=>{
+        console.log(data)
+    })
+
